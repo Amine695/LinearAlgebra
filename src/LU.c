a2e06d6ef66a7e1e731e2a27782c6e3b0349bb38
Amine <amineberbagui@outlook.fr>
2022-05-07 16:59:35 +0200

    Update LU

--- src/LU.c
+++ src/LU.c
@@ -187,18 +187,11 @@ void pivot(matrix a, matrix p, DIM)
 }
 
 /**
- *  @brief Compute the LU decomposition using the pivot function
+ *  @brief Compute the LU decomposition of P*A 
 */
-void LU(matrix A, matrix L, matrix U, matrix P, DIM,prime)
+void LU(matrix A, matrix L, matrix U,matrix P, DIM,prime)
 {
-    // On initialise L et U à 0
-    null_matrix(L,n);
-    null_matrix(U,n);
-
-    // calcul la matrice de permutation P 
-    pivot(A,P,n);
-
-
+    
     int tmp = 0,tmp2 = 0;
     for_(i,n)
         L[i][i] = 1; // identity matrix
@@ -238,7 +231,6 @@ void LU(matrix A, matrix L, matrix U, matrix P, DIM,prime)
             }
         }
 
-    //freeMatrix(Abis);
 
 }
 
@@ -285,20 +277,23 @@ void correction(matrix A, matrix L, matrix U, matrix P, DIM, prime)
 
 /**
  *  @brief Run the LU decomposition to make the main function cleaner
+ *  It uses the pivot function to compute the permutation matrix
 */
 void RunLU(matrix A, matrix P, matrix L, matrix U, DIM, prime)
 {
     printf("************** LU decomposition ***************\n");
-
+    // calcul the permutation matrix
+    pivot(A,P,n);
+    // compute P*A 
+    matrix PA = matmul(P,A,n,p);
     // Computation and time measurements
     double start,end,time_elapsed;
     start = clock();
-    LU(A,L,U,P,n,p);
+    LU(PA,L,U,P,n,p); // Here PA = P*A so it computes PA = LU
     end = clock();
     time_elapsed = ((double)end - start) / CLOCKS_PER_SEC;
     
     // Show results
-   
     displayMatrix("A matrix",A,n);
     displayMatrix("L matrix",L,n);
     displayMatrix("U matrix",U,n);
@@ -307,4 +302,6 @@ void RunLU(matrix A, matrix P, matrix L, matrix U, DIM, prime)
      // Check if PA == LU
     correction(A,L,U,P,n,p);
     printf("Time duration with n = %d : %f s\n",n,time_elapsed);
+
+    freeMatrix(PA);
 }
